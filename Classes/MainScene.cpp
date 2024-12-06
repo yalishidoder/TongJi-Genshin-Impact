0a6e8a9da198289c2c22ee01d7c19167ac77feef
orange123878 <1753553833@qq.com>
2024-12-06 16:16:20 +0800

    class
    目前实现主地图加载

---
+++ Classes/MainScene.cpp
@@ -0,0 +1,113 @@
+#include "MainScene.h"
+#include "SimpleAudioEngine.h"
+
+USING_NS_CC;
+
+Scene* MainScene::createScene()
+{
+    return MainScene::create();
+}
+
+// Print useful error message instead of segfaulting when files are not there.
+static void problemLoading(const char* filename)
+{
+    printf("Error while loading: %s\n", filename);
+    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in MainSceneScene.cpp\n");
+}
+
+// on "init" you need to initialize your instance
+bool MainScene::init()
+{
+    if (!Scene::init())
+    {
+        return false;
+    }
+
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    /////////////////////////////
+    // add a "close" icon to exit the progress. it's an autorelease object
+    auto closeItem = MenuItemImage::create(
+        "CloseNormal.png",
+        "CloseSelected.png",
+        CC_CALLBACK_1(MainScene::menuCloseCallback, this));
+
+    if (closeItem == nullptr ||
+        closeItem->getContentSize().width <= 0 ||
+        closeItem->getContentSize().height <= 0)
+    {
+        problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
+    }
+    else
+    {
+        float x = origin.x + visibleSize.width - closeItem->getContentSize().width / 2;
+        float y = origin.y + closeItem->getContentSize().height / 2;
+        closeItem->setPosition(Vec2(x, y));
+    }
+
+    // create menu, it's an autorelease object
+    auto menu = Menu::create(closeItem, NULL);
+    menu->setPosition(Vec2::ZERO);
+    this->addChild(menu, 1);
+    /////////////////////////////
+    // 加载主地图
+
+    std::string file = "mainmap.tmx";
+    map = TMXTiledMap::create(file);
+
+    if (!map) {
+        CCLOG("Failed to load TMX map: %s", file.c_str());
+        return false;
+    }
+    CCLOG("Map loaded successfully!");
+
+    // 获取地图尺寸
+    float mapWidth = map->getMapSize().width * map->getTileSize().width;
+    float mapHeight = map->getMapSize().height * map->getTileSize().height;
+
+    // 计算缩放比例
+    float scaleX = visibleSize.width / mapWidth;
+    float scaleY = visibleSize.height / mapHeight;
+    float scale = std::min(scaleX, scaleY);
+    map->setScale(scale);
+
+    map->setAnchorPoint(Vec2(0.5f, 0.5f));
+    map->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+    this->addChild(map);
+
+
+    // add a label shows "MainScene"
+
+    auto label = Label::createWithTTF("MainScene", "fonts/Marker Felt.ttf", 24);
+    if (label == nullptr)
+    {
+        problemLoading("'fonts/Marker Felt.ttf'");
+    }
+    else
+    {
+        // position the label on the center of the screen
+        label->setPosition(Vec2(origin.x + visibleSize.width / 2,
+            origin.y + visibleSize.height - label->getContentSize().height));
+
+        // add the label as a child to this layer
+        this->addChild(label, 1);
+    }
+
+   
+    return true;
+}
+
+
+void MainScene::menuCloseCallback(Ref* pSender)
+{
+    //Close the cocos2d-x game scene and quit the application
+    Director::getInstance()->end();
+
+    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() as given above,instead trigger a custom event created in RootViewController.mm as below*/
+
+    //EventCustom customEndEvent("game_scene_close_event");
+    //_eventDispatcher->dispatchEvent(&customEndEvent);
+
+
+}
